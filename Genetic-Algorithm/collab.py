fc5e0aac816c0f784e829ec2bbfaba240e06c921
Nikos Soultanis <fosdrogg@gmail.com>
2020-06-15 21:55:46 +0300

    Add files via upload

--- Genetic-Algorithm/collab.py
+++ Genetic-Algorithm/collab.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pygad
 from pyeasyga import pyeasyga
 from pyeasyga.pyeasyga import GeneticAlgorithm
+import matplotlib.pyplot as plt
 
 # TODO
 # random_user selection -> tick
@@ -44,7 +45,7 @@ def select_random_user(data):  # data is a dataframe
     # returns all the ratings of th random user
     return random_user, random_user_ratings
 
-
+fitness_list = list()
 # check corcoef value range
 def fitness_function(chromosome, solution_index):  # get the chromosome
     top10 = most_similar  # retrieve to top users from the other function
@@ -52,10 +53,11 @@ def fitness_function(chromosome, solution_index):  # get the chromosome
     for i in range(10):  # get only the top 10
         top_users.append(top10[i][0])
     r = list()
+
     most_fit = None
     for i in range(10):  # calculate the pearson helper for each other user
-        neighbor = calculate_pearson_helper(top_users[i])
-        prod = abs(np.corrcoef(chromosome, neighbor)[0, 1])  # and now calculate the corrcoef
+        neighbor = prods[i]
+        prod = (np.corrcoef(chromosome, neighbor)[0, 1])  # and now calculate the corrcoef
         r.append(prod)
     fit = np.mean(r)
     if most_fit is None:  # first iteration
@@ -64,7 +66,7 @@ def fitness_function(chromosome, solution_index):  # get the chromosome
         most_fit = fit
     elif fit < most_fit:
         most_fit = most_fit
-
+    fitness_list.append(most_fit)
     return most_fit  # return the fitness value
 
 # add MEAN values to the missing ratings
@@ -92,17 +94,19 @@ def calculate_pearson_helper2(data, rand_user_id): # same logic as before but us
 
 # find the neighborhood
 def calculate_correlation(rand_userid, users):  # function to calculate the corrcoef and finally the most similar users
-    global most_similar
+    global most_similar, prods
     saved_correlations = list()
     matches = list()
     pearson_cor = calculate_pearson_helper(rand_userid)
     # remove the randomly selected user
     neighbour_users = np.delete(users, np.where(users == rand_userid))
     # calculate each user pearson
+    prods = list()
     for usr in neighbour_users:
         pearson_prod = calculate_pearson_helper(usr)
         # save pearson to list
         saved_correlations.append((usr, pearson_prod))
+        prods.append(pearson_prod)
     # for each user's pearson calculate the correlation
     for item in saved_correlations:
         matched_user = item[0]
@@ -117,7 +121,7 @@ def calculate_correlation(rand_userid, users):  # function to calculate the corr
 
 
 def generate_mutation(offspring_crossover): # mut_prob is the probability of mutation
-    mut_prob = 1
+    mut_prob = 0
     roll = random.randrange(1, 100)
     if roll <= mut_prob:
         index = random.randrange(len(offspring_crossover))
@@ -125,13 +129,13 @@ def generate_mutation(offspring_crossover): # mut_prob is the probability of mut
         offspring_crossover[index] = offspring_crossover[index] - mutated_rating
         if offspring_crossover[index] < 0:
             offspring_crossover[index] = offspring_crossover[index] * (-1)
-        return offspring_crossover
+        return repair_procedure(user_rating, offspring_crossover)
     else:
         return offspring_crossover
 
 
 def perform_crossover(m, f):  # m is mother and f is father
-    cross_prob = 90 # probability of crossover
+    cross_prob = 60 # probability of crossover
     roll = random.randrange(1, 100)
     if roll <= cross_prob:
         index = random.randrange(1, len(m))
@@ -174,10 +178,10 @@ def callback_generation(ga_instance): # helper functions for the second library 
     print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1] * 100))
     print("Change     = {change}".format(change=(ga_instance.best_solution()[1] - last_fitness) * 100))
     solution, solution_fitness, solution_idx = ga_instance.best_solution()
-    if (ga_instance.best_solution()[1] - last_fitness) * 100 < 0.1:
-        print("Terminated because it doesn't progress.")
-        ga.plot_result()
-        quit()
+    # if (ga_instance.best_solution()[1] - last_fitness) * 100 < 0.1:
+    #     print("Terminated because it doesn't progress.")
+    #     ga.plot_result()
+    #     quit()
     if ga_instance.generations_completed > 201:
         print("Too many generations.")
         ga.plot_result()
@@ -190,7 +194,7 @@ global user_rating
 random_user, random_user_ratings = select_random_user(df) # get the random user
 user_rating = random_user_ratings # save to global var the ratings
 
-population = initialize_population(200) # initialize population of n-chromosomes
+population = initialize_population(2) # initialize population of n-chromosomes
 fixed_population = list()
 for chromosome in population:
     fixed_population.append(repair_procedure(random_user_ratings, chromosome)) # fix the chromosomes
@@ -198,16 +202,19 @@ print("Calculating Pearson")
 calculate_pearson_helper2(df, random_user)  # user first or second method to fill the values
 print("Calculating Correlation")
 x = calculate_correlation(random_user, users)  # corrcoef calculations
-# print(most_similar)
+print(most_similar)
+for i in range(10):
+    prods[i] = repair_procedure(user_rating, prods[i])
 print("Training")
 
 # BEGIN TRAINING
 # --------- First Library --------- #
 # uncomment to run one of each libraries
 initial = random_chromosome(movies)
+generations = 40
 ga = pyeasyga.GeneticAlgorithm(initial,
                                population_size=20,
-                               generations=30,
+                               generations=generations,
                                maximise_fitness=True,
                                elitism=True)
 
@@ -218,24 +225,34 @@ ga.mutate_function = generate_mutation
 ga.run()
 
 print(ga.best_individual())
+counter = 0
+fit_list = list()
+print(len(fitness_list))
+for i in range(0, len(fitness_list), generations):
+    fit_list.append(sum(fitness_list[i: i+10])/10)
+    counter = counter + 1
+plt.plot(fit_list)
+plt.ylabel('Fitness')
+plt.xlabel('Generations')
+plt.show()
 best_match = ga.best_individual()[1]
 best_match = repair_procedure(random_user_ratings, best_match)
 print(fitness_function(best_match, 1))
-# --------- First Library --------- #
 
 
 # --------- Second Library --------- #
 # uncomment to run one of each libraries
-# ga = pygad.GA(num_generations=5,
-#               num_parents_mating=5,
+#
+# ga = pygad.GA(num_generations=1000,
+#               num_parents_mating=2,
 #               initial_population=fixed_population,
 #               init_range_low=1,
 #               init_range_high=5,
 #               fitness_func=fitness_function,
 #               parent_selection_type="sss",
-#               crossover_type="uniform",
-#               mutation_type="random",
-#               mutation_percent_genes=20,
+#               crossover_type="single_point",
+#               mutation_type="swap",
+#               mutation_percent_genes=1,
 #               callback_generation=callback_generation)
 # ga.run()
 # ga.plot_result()
